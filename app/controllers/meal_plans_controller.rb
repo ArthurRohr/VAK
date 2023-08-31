dc906c3c38e3998772cd31e83f526dcaa92ffc5e
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2023-08-31 15:42:55 +0200

    Redis and sidekiq setup

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -6,7 +6,7 @@ class MealPlansController < ApplicationController
 
   def show
     # @meal_plan = MealPlan.find(params[:id])
-    get_new_plan_api
+    ai_meal_plan
   end
 
   def new
@@ -23,33 +23,65 @@ class MealPlansController < ApplicationController
     end
   end
 
-  def get_new_plan_api
-    response = HTTParty.get('https://api.spoonacular.com/mealplanner/generate', {
-      query: {
-        apiKey: ENV['MEAL_PLAN_API_KEY'],
-        timeFrame: 'week',
-        targetCalories: 2000,
-        diet: 'vegetarian',
-        exclude: 'shellfish,olives'
-      }
-    })
-    @data = JSON.parse(response.body)
-    week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-    week.each do |day|
-      @data["week"][day].each do |meals|
-        meals[1].each do |meal|
-          if meal.is_a?Hash
-            id = meal["id"]
-            recipe_response = HTTParty.get("https://api.spoonacular.com/recipes/#{id}/information",{
-              query: {
-                apiKey: ENV['MEAL_PLAN_API_KEY'],
-                includeNutrition: true
-              }
-            })
-            @recipe_data = JSON.parse(recipe_response.body)
-          end
-        end
-      end
-    end
+  def edit
+    @meal_plan = MealPlan.find(params[:id])
+  end
+
+  def ai_meal_plan
+    # @days = 2
+    @diet = "vegan"
+    # @alergies = ['milk']
+    json_format = {
+      "days":[
+          {
+            "day":{
+                "breakfast":{
+                  "title":"",
+                  "ingredients":""
+                },
+                "lunch":{
+                  "title":"",
+                  "ingredients":""
+                },
+                "dinner":{
+                  "title":"",
+                  "ingredients":""
+                }
+            }
+          }
+      ]
+    }.to_s
+
+    @response = JSON.parse(OpenaiService.new("create a #{@diet} meals plan for a day with recipes in the following json format " + json_format).call)
   end
 end
+
+  # def get_new_plan_api
+  #   response = HTTParty.get('https://api.spoonacular.com/mealplanner/generate', {
+  #     query: {
+  #       apiKey: ENV['MEAL_PLAN_API_KEY'],
+  #       timeFrame: 'week',
+  #       targetCalories: 2000,
+  #       diet: 'vegetarian',
+  #       exclude: 'shellfish,olives'
+  #     }
+  #   })
+  #   @data = JSON.parse(response.body)
+  #   week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+  #   week.each do |day|
+  #     @data["week"][day].each do |meals|
+  #       meals[1].each do |meal|
+  #         if meal.is_a?Hash
+  #           id = meal["id"]
+  #           recipe_response = HTTParty.get("https://api.spoonacular.com/recipes/#{id}/information",{
+  #             query: {
+  #               apiKey: ENV['MEAL_PLAN_API_KEY'],
+  #               includeNutrition: true
+  #             }
+  #           })
+  #           @recipe_data = JSON.parse(recipe_response.body)
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
