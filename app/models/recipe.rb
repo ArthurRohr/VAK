a9220f0f6f04a3d7a7f7e36e2bcfb523f23eeb10
linda <lindawilson.may21@gmail.com>
2023-09-04 15:02:17 +0200

    image for AI recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
   has_many :users, through: :bookmarks
   has_many :meal_plans, through: :meal_plan_recipes
 
-  has_one_attached :pictures
+  has_one_attached :picture
   attribute :ai_created, default: false
 
   CUISINE = ["Italian", "Indian", "Mexican", "Japanese", "Chinese", "French", "Thai", "Mediterranean", "American", "Greek"]
--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -13,12 +13,18 @@ class OpenaiService
     response = client.chat(
       parameters: {
           model: "gpt-3.5-turbo", # Required.
-          messages: [{ role: "user", content: prompt }], # Required.
+          messages: [{ role: "user", content: @prompt }], # Required.
           temperature: 0.7,
           stream: false,
 					max_tokens: 1000 # might want to check this
       })
     # you might want to inspect the response and see what the api is giving you
     return response["choices"][0]["message"]["content"]
+  end
+
+
+  def getImageUrl
+    response = client.images.generate(parameters: { prompt: @prompt, size: "256x256" })
+    response.dig("data", 0, "url")
   end
 end
