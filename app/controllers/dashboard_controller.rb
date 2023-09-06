97ce3e041a617192b742ba2b5fbe466c9c9bead0
arthur rohr <arthur.rohr.pcc@gmail.com>
2023-09-06 15:45:24 +0200

    styling the rating on the card

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def dashboard
     @recipes = user_recipes
+    @recipes_created = Recipe.where(user_id: current_user.id, ai_created: false)
     @meal_plans = user_meal_plans
     @bookmarks = Bookmark.where(user_id: current_user.id)
     @recipe_count = Recipe.where(user_id: current_user.id, ai_created: false).count
